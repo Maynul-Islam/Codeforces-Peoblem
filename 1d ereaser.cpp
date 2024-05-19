8e83c40e1d8786f2e1f7207961726f1ad718ceb0
Maynul Islam Munna <133635068+Maynul-Islam@users.noreply.github.com>
2024-05-19 20:15:41 +0600

    codeforces problem 1d ereaser solution

---
+++ 1d ereaser.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        string s;
+        cin>>s;
+        int ans = 0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='B')
+            {
+                ans++;
+                i = i + (k-1);
+            }
+        }
+        cout << ans <<endl;
+    }
+    return 0;
+}
